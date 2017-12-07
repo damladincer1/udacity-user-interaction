a38a07d80139bcb29a315bee8d1619979a3faf0b
damladincer1 <damladincer1@gmail.com>
2017-12-07 21:29:19 +0200

    Contact manager with menu

--- src/com/damladincer/oop/contactmanager/Contact.java
+++ src/com/damladincer/oop/contactmanager/Contact.java
@@ -1,43 +1,44 @@
 package com.damladincer.oop.contactmanager;
 
 public class Contact {
-	
-	//Fields
+
+	// Fields
 	private String name;
 	private String email;
 	private String phoneNumber;
-	
-	//Constructors
+
+	// Constructors
 	public Contact() {
-		
+
 	}
+
 	public Contact(String name, String email, String phoneNumber) {
 		this.name = name;
 		this.email = email;
 		this.phoneNumber = phoneNumber;
 	}
-	
-	//Methods
+
+	// Getter & Setter
 	public String getName() {
 		return name;
 	}
-	
+
 	public void setName(String name) {
 		this.name = name;
 	}
-	
+
 	public String getEmail() {
 		return email;
 	}
-	
+
 	public void setEmail(String email) {
 		this.email = email;
 	}
-	
+
 	public String getPhoneNumber() {
 		return phoneNumber;
 	}
-	
+
 	public void setPhoneNumber(String phoneNumber) {
 		this.phoneNumber = phoneNumber;
 	}
--- src/com/damladincer/oop/contactmanager/ContactManager.java
+++ src/com/damladincer/oop/contactmanager/ContactManager.java
@@ -1,39 +1,135 @@
 package com.damladincer.oop.contactmanager;
 
+import java.util.Scanner;
+
 public class ContactManager {
-	private Contact [] contacts;
+	// Fields
+	public final int SIZE = 100;
+	
+	private Contact[] contacts;
 	private int friendsCount;
 
+	private Scanner scanner;
+
+	// Constructor
 	public ContactManager() {
-		this.contacts = new Contact[100];
-		this.friendsCount=0;	
+		this.contacts = new Contact[SIZE];
+		this.friendsCount = 0;
+		this.scanner = new Scanner(System.in);
 	}
 
+	// Methods
+
+	public void addContact() {
+		Contact contact = new Contact();
+		System.out.println("Enter name of contact:");
+		String name = scanner.nextLine();
+		contact.setName(name);
+
+		System.out.println("Please enter the number of contact:");
+		String phone = scanner.nextLine();
+		contact.setPhoneNumber(phone);
+
+		System.out.println("Please enter the e-mail of contact:");
+		String email = scanner.nextLine();
+		contact.setEmail(email);
+
+		this.contacts[friendsCount] = contact;
+		this.friendsCount++;
+
+		System.out.println(friendsCount + " contact added to list");
+
+		printMenu();
+	}
+
+	public void searchContact(String name) {
+		for (int i = 0; i < friendsCount; i++) {
+			if (contacts[i].getName().toUpperCase().contains(name.toUpperCase())) {
+				System.out.println(contacts[i].getName() + "-" + contacts[i].getPhoneNumber());
+			}
+		}
+		printMenu();
+	}
+
+	public void deleteContact(String name) {
+		Contact[] newContactList = new Contact[SIZE];
+		int j = 0;
+		for (int i = 0; i < this.friendsCount; i++) {
+			if (this.contacts[i].getName().toUpperCase().equals(name.toUpperCase())) {
+				this.contacts[i] = null;
+				System.out.println("The contact deleted");
+				this.friendsCount--;
+			} else {
+				newContactList[j] = this.contacts[i];
+				j++;
+			}
+		}
+		
+		
+		
+		this.contacts = newContactList;
+		printMenu();
+	}
+
+	public void listContacts() {
+		if (friendsCount != 0) {
+			for (int i = 0; i < friendsCount; i++) {
+				System.out.println(
+						contacts[i].getName() + "-" + contacts[i].getPhoneNumber() + "-" + contacts[i].getEmail());
+			}
+			printMenu();
+		} else {
+			System.out.println("There is no contact in your list. ");
+			System.out.println("Please add a contact");
+			printMenu();
+		}
+	}
+
+	public void printMenu() {
+		System.out.println("");
+		System.out.println("1 - Add New Contact\r\n" + "2 - Search Contact\r\n" + "3 - Delete Contact\r\n"
+				+ "4 - List All Contacts\r\n" + "5 - Exit the Program\r\n" + "");
+		System.out.println("Please choose the operation you want");
+		scanMenuItem();
+	}
+
+	public void scanMenuItem() {
+		Scanner scan = new Scanner(System.in);
+		int i = scan.nextInt();
+		switch (i) {
+		case 1:
+			addContact();
+			break;
+		case 2:
+			System.out.println("Please enter the contact name you want to search:");
+			String name = scanner.nextLine();
+			searchContact(name);
+			break;
+		case 3:
+			System.out.println("Please enter the contact name:");
+			String name1 = scanner.nextLine();
+			deleteContact(name1);
+			break;
+		case 4:
+			listContacts();
+			break;
+		case 5:
+			scanner.close();
+			System.exit(0);
+			break;
+		default:
+			System.out.println("Choose the correct operation");
+			break;
+		}
+		scan.close();
+	}
+
+	// Getter & Setter
 	public Contact[] getContacts() {
 		return contacts;
 	}
+
 	public int getFriendsCount() {
 		return friendsCount;
 	}
-	
-	public void addContact(Contact contact) {
-		this.contacts[friendsCount]=contact;
-		this.friendsCount++;
-	}
-	
-	public Contact searchContact(String name) {
-		for(int i = 0;i<friendsCount;i++) {
-			if(contacts[i].getName().equals(name)) {
-				return contacts[i];
-			}
-		} 
-		return null;
-	}
-	
-	public void printAllContacts() {
-		for(int i=0;i<friendsCount;i++) {
-			System.out.println(contacts[i].getName()+"-" +contacts[i].getPhoneNumber());
-		}
-	}
-	
 }
